e3f0d43580c71f9278bdd98f7259fc2fe846f7f1
AlexSosnkowski <alexsosnkowski@gmail.com>
2025-04-24 15:18:39 -0400

    website logo

--- audit_project/audit_project/urls.py
+++ audit_project/audit_project/urls.py
@@ -22,6 +22,12 @@ from audit_app.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", PortalUploadView.as_view(), name="portal_upload"),
@@ -32,5 +38,3 @@ urlpatterns = [
     path('pca-data/', pca_view, name='pca_data'),
     path('simulate-attack/', simulate_attack_view, name='simulate_attack'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
